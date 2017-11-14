1e238763a89815f007724aeb481468c65b8fd28a
Adwait Tumbde <atumbde@linkedin.com>
2017-11-14 09:10:16 -0800

    Fixed typo in variable name

--- coral-calcite/src/main/java/com/linkedin/coral/Dummy.java
+++ coral-calcite/src/main/java/com/linkedin/coral/Dummy.java
@@ -4,7 +4,7 @@ package com.linkedin.coral;
 // TODO: REMOVE ME. This class exists only to trigger build for this module.
 // There should be a cleaner way in gradle to achieve this.
 public class Dummy {
-  private static final String PLACEHOLDER_StRING = "";
+  private static final String PLACEHOLDER_STRING = "";
   private Dummy() {
 
   }
