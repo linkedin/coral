0748176b1e889c6e37182c8bf74c3472e8dde75d
Wenye Zhang <wyzhang@linkedin.com>
2019-10-29 10:19:45 -0700

    LIHADOOP-49753: Add license header to files
    RB=1875736
    BUG=LIHADOOP-49753
    G=dali-reviewers
    R=atumbde,wmoustaf,smahadik,khtran,rratti
    A=wmoustaf

--- coral-pig/src/main/java/com/linkedin/coral/pig/rel2pig/exceptions/IllegalPigCastException.java
+++ coral-pig/src/main/java/com/linkedin/coral/pig/rel2pig/exceptions/IllegalPigCastException.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2019 LinkedIn Corporation. All rights reserved.
+ * Licensed under the BSD-2 Clause license.
+ * See LICENSE in the project root for license information.
+ */
 package com.linkedin.coral.pig.rel2pig.exceptions;
 
 import com.linkedin.coral.pig.rel2pig.rel.operators.PigCastFunction;
