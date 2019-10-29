912ecdf1f0c1f3cb67ac54f9fe7701093f9792c9
Wenye Zhang <wyzhang@linkedin.com>
2019-10-29 10:19:45 -0700

    LIHADOOP-49753: Add license header to files
    RB=1858588
    BUG=LIHADOOP-49753
    G=dali-reviewers
    R=khtran,smahadik,wmoustaf,atumbde,rratti
    A=wmoustaf

--- coral-spark/src/main/java/com/linkedin/coral/spark/CoralSpark.java
+++ coral-spark/src/main/java/com/linkedin/coral/spark/CoralSpark.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2019 LinkedIn Corporation. All rights reserved.
+ * Licensed under the BSD-2 Clause license.
+ * See LICENSE in the project root for license information.
+ */
 package com.linkedin.coral.spark;
 
 import com.linkedin.coral.spark.containers.SparkRelInfo;
