5ea8564757604896b3a04e39128b1cb55be497fb
Sushant Raikar <sraikar@linkedin.com>
2020-02-18 08:36:52 -0800

    CORAL-95,MPTRUNKDEV-105532: Reverting Parseability revet, Adding Licence Information
    RB=1972906
    G=dali-reviewers
    R=khtran,wmoustaf,rratti,atumbde,smahadik
    A=smahadik

---
+++ coral-tools/src/main/java/com/linkedin/coral/tools/SparkValidator.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2019 LinkedIn Corporation. All rights reserved.
+ * Licensed under the BSD-2 Clause license.
+ * See LICENSE in the project root for license information.
+ */
+package com.linkedin.coral.tools;
+
+import com.linkedin.coral.hive.hive2rel.HiveMetastoreClient;
+import com.linkedin.coral.hive.hive2rel.HiveToRelConverter;
+import com.linkedin.coral.spark.CoralSpark;
+import java.io.PrintWriter;
+import org.apache.calcite.rel.RelNode;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.catalyst.parser.ParseException;
+
+
+public class SparkValidator implements LanguageValidator {
+  @Override
+  public String getCamelName() {
+    return "SparkSQL";
+  }
+
+  @Override
+  public String getStandardName() {
+    return "Spark SQL";
+  }
+
+  @Override
+  public void convertAndValidate(String db, String table, HiveMetastoreClient hiveMetastoreClient,
+      PrintWriter outputWriter) {
+    SparkSession spark = SparkSession
+        .builder()
+        .appName("CoralTools")
+        .config("spark.master", "local")
+        .getOrCreate();
+    spark.sparkContext().setLogLevel("ERROR");
+    CoralSpark coralSparkTranslation = getCoralSparkTranslation(db, table, hiveMetastoreClient);
+    validateSparkSql(spark, coralSparkTranslation);
+    if (outputWriter != null) {
+      outputWriter.println(db + "." + table + ":");
+      outputWriter.println(coralSparkTranslation.getSparkSql());
+      outputWriter.flush();
+    }
+  }
+
+  private void validateSparkSql(SparkSession spark, CoralSpark coralSparkTranslation) {
+    String sql = coralSparkTranslation.getSparkSql();
+    try {
+      spark.sessionState().sqlParser().parsePlan(sql);
+    } catch (ParseException e) {
+      throw new RuntimeException("Failed to parse spark sql: " + sql, e);
+    }
+  }
+
+  CoralSpark getCoralSparkTranslation(String db, String table, HiveMetastoreClient hiveMetastoreClient) {
+    final HiveToRelConverter hiveToRelConverter = HiveToRelConverter.create(hiveMetastoreClient);
+    final RelNode rel = hiveToRelConverter.convertView(db, table);
+    return CoralSpark.create(rel);
+  }
+}
