c2e5a679d554e5b6f0fde238fc68f3fbaa392550
Wenye Zhang <wyzhang@linkedin.com>
2021-08-02 22:55:13 -0700

    coral-schema: merge schema if avro schema is outdated (#92)
    * merge schema if avro schema is outdated
    
    * add enum and fixed type support
    
    * assign null default properly for new fields
    
    * fix spotlessJavaCheck
    
    * add unit tests
    
    * add unit tests
    
    * handle union type during merging
    
    * address comments and add more unit tests
    
    Co-authored-by: Wenye Zhang <wyzhang@wyzhang-mn1.linkedin.biz>

---
+++ coral-schema/src/main/java/com/linkedin/coral/schema/avro/AvroSchemaVisitor.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright 2021 LinkedIn Corporation. All rights reserved.
+ * Licensed under the BSD-2 Clause license.
+ * See LICENSE in the project root for license information.
+ */
+package com.linkedin.coral.schema.avro;
+
+import java.util.Deque;
+import java.util.List;
+
+import org.apache.avro.Schema;
+
+import com.linkedin.coral.com.google.common.base.Preconditions;
+import com.linkedin.coral.com.google.common.collect.Lists;
+
+
+public abstract class AvroSchemaVisitor<T> {
+  public static <T> T visit(Schema schema, AvroSchemaVisitor<T> visitor) {
+    switch (schema.getType()) {
+      case RECORD:
+        // check to make sure this hasn't been visited before
+        String name = schema.getFullName();
+        Preconditions.checkState(!visitor.recordLevels.contains(name), "Cannot process recursive Avro record %s", name);
+
+        visitor.recordLevels.push(name);
+
+        List<Schema.Field> fields = schema.getFields();
+        List<String> names = Lists.newArrayListWithExpectedSize(fields.size());
+        List<T> results = Lists.newArrayListWithExpectedSize(fields.size());
+        for (Schema.Field field : schema.getFields()) {
+          names.add(field.name());
+          T result = visitWithName(field.name(), field.schema(), visitor);
+          results.add(result);
+        }
+
+        visitor.recordLevels.pop();
+
+        return visitor.record(schema, names, results);
+
+      case UNION:
+        List<Schema> types = schema.getTypes();
+        List<T> options = Lists.newArrayListWithExpectedSize(types.size());
+        for (Schema type : types) {
+          options.add(visit(type, visitor));
+        }
+        return visitor.union(schema, options);
+
+      case ARRAY:
+        // TODO: handle logical map
+        return visitor.array(schema, visitWithName("element", schema.getElementType(), visitor));
+
+      case MAP:
+        return visitor.map(schema, visitWithName("value", schema.getValueType(), visitor));
+
+      default:
+        return visitor.primitive(schema);
+    }
+  }
+
+  private Deque<String> recordLevels = Lists.newLinkedList();
+  private Deque<String> fieldNames = Lists.newLinkedList();
+
+  protected Deque<String> fieldNames() {
+    return fieldNames;
+  }
+
+  private static <T> T visitWithName(String name, Schema schema, AvroSchemaVisitor<T> visitor) {
+    try {
+      visitor.fieldNames.addLast(name);
+      return visit(schema, visitor);
+    } finally {
+      visitor.fieldNames.removeLast();
+    }
+  }
+
+  public T record(Schema record, List<String> names, List<T> fields) {
+    return null;
+  }
+
+  public T union(Schema union, List<T> options) {
+    return null;
+  }
+
+  public T array(Schema array, T element) {
+    return null;
+  }
+
+  public T map(Schema map, T value) {
+    return null;
+  }
+
+  public T primitive(Schema primitive) {
+    return null;
+  }
+}
