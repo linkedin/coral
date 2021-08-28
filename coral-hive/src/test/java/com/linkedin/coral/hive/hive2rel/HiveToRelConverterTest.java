7c3ed349f8fc614683edcd5224a3835ab0bbc8b3
Zheng Shao <zheng@uber.com>
2021-08-27 17:51:41 -0700

    Bug fixes

--- coral-hive/src/test/java/com/linkedin/coral/hive/hive2rel/HiveToRelConverterTest.java
+++ coral-hive/src/test/java/com/linkedin/coral/hive/hive2rel/HiveToRelConverterTest.java
@@ -52,7 +52,7 @@ public class HiveToRelConverterTest {
 
   public void testBasicWithSQL(String sql) {
     RelNode rel = converter.convertSql(sql);
-    RelBuilder relBuilder = createRelBuilderHiveRelBuilder();
+    RelBuilder relBuilder = createRelBuilder();
     RelNode expected = relBuilder.scan(ImmutableList.of("hive", "default", "foo"))
         .project(ImmutableList.of(relBuilder.field("a"), relBuilder.field("b"), relBuilder.field("c")),
             ImmutableList.of(), true)
