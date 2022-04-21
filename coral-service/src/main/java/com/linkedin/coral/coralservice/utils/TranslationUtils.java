1ce86cb97ea818499ca1ac478c258cca33953f51
kge <kevingehd@gmail.com>
2022-04-21 12:07:34 -0400

    fixing nits

--- coral-service/src/main/java/com/linkedin/coral/coralservice/utils/TranslationUtils.java
+++ coral-service/src/main/java/com/linkedin/coral/coralservice/utils/TranslationUtils.java
@@ -3,11 +3,11 @@ package com.linkedin.coral.coralservice.utils;
 import com.linkedin.coral.spark.CoralSpark;
 import org.apache.calcite.rel.RelNode;
 
-import static com.linkedin.coral.coralservice.utils.CoralUtils.*;
+import static com.linkedin.coral.coralservice.utils.CoralProvider.*;
 
 
 public class TranslationUtils {
-  
+
   public static String translateTrinoToSpark(String query) {
     RelNode relNode = trinoToRelConverter.convertSql(query);
     CoralSpark coralSpark = CoralSpark.create(relNode);
