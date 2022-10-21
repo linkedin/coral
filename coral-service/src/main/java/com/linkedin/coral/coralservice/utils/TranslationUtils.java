ab19dd9df88c3d66087b5b640e775805bd3cae3e
Jiefan Li <jiefli@linkedin.com>
2022-10-21 15:16:36 -0400

    Coral-Service: Reinstantiate converters for each translation (#318)

--- coral-service/src/main/java/com/linkedin/coral/coralservice/utils/TranslationUtils.java
+++ coral-service/src/main/java/com/linkedin/coral/coralservice/utils/TranslationUtils.java
@@ -7,7 +7,10 @@ package com.linkedin.coral.coralservice.utils;
 
 import org.apache.calcite.rel.RelNode;
 
+import com.linkedin.coral.hive.hive2rel.HiveToRelConverter;
 import com.linkedin.coral.spark.CoralSpark;
+import com.linkedin.coral.trino.rel2trino.RelToTrinoConverter;
+import com.linkedin.coral.trino.trino2rel.TrinoToRelConverter;
 
 import static com.linkedin.coral.coralservice.utils.CoralProvider.*;
 
@@ -15,18 +18,18 @@ import static com.linkedin.coral.coralservice.utils.CoralProvider.*;
 public class TranslationUtils {
 
   public static String translateTrinoToSpark(String query) {
-    RelNode relNode = trinoToRelConverter.convertSql(query);
+    RelNode relNode = new TrinoToRelConverter(hiveMetastoreClient).convertSql(query);
     CoralSpark coralSpark = CoralSpark.create(relNode);
     return coralSpark.getSparkSql();
   }
 
   public static String translateHiveToTrino(String query) {
-    RelNode relNode = hiveToRelConverter.convertSql(query);
-    return relToTrinoConverter.convert(relNode);
+    RelNode relNode = new HiveToRelConverter(hiveMetastoreClient).convertSql(query);
+    return new RelToTrinoConverter().convert(relNode);
   }
 
   public static String translateHiveToSpark(String query) {
-    RelNode relNode = hiveToRelConverter.convertSql(query);
+    RelNode relNode = new HiveToRelConverter(hiveMetastoreClient).convertSql(query);
     CoralSpark coralSpark = CoralSpark.create(relNode);
     return coralSpark.getSparkSql();
   }
