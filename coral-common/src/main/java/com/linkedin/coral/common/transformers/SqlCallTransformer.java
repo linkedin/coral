ab8f6496dc42a9b7045e409e0ef4e5eab397d9f9
Yiqiang Ding <yiqding@yiqding-mn1.linkedin.biz>
2023-02-08 12:35:06 -0800

    add {@link} in javadoc of a function

--- coral-common/src/main/java/com/linkedin/coral/common/transformers/SqlCallTransformer.java
+++ coral-common/src/main/java/com/linkedin/coral/common/transformers/SqlCallTransformer.java
@@ -105,7 +105,7 @@ public abstract class SqlCallTransformer {
   }
 
   /**
-   * This function creates a SqlOperator for a function with the function name and return type inference.
+   * This function creates a {@link SqlOperator} for a function with the function name and return type inference.
    */
   protected static SqlOperator createSqlOperator(String functionName, SqlReturnTypeInference typeInference) {
     SqlIdentifier sqlIdentifier = new SqlIdentifier(ImmutableList.of(functionName), SqlParserPos.ZERO);
