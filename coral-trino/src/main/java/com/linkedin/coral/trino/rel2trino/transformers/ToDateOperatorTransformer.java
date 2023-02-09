b713c40e3047b37c54e644dad5339b043d502045
Yiqiang Ding <yiqding@yiqding-mn1.linkedin.biz>
2023-02-09 14:58:04 -0800

    Coral-Trino: replace TIMESTAMP_OPERATOR with SqlCastFunction in ToDateOperatorTransformer

--- coral-trino/src/main/java/com/linkedin/coral/trino/rel2trino/transformers/ToDateOperatorTransformer.java
+++ coral-trino/src/main/java/com/linkedin/coral/trino/rel2trino/transformers/ToDateOperatorTransformer.java
@@ -9,16 +9,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.calcite.sql.SqlCall;
-import org.apache.calcite.sql.SqlIdentifier;
+import org.apache.calcite.sql.SqlLiteral;
 import org.apache.calcite.sql.SqlNode;
-import org.apache.calcite.sql.SqlOperator;
-import org.apache.calcite.sql.SqlWriter;
+import org.apache.calcite.sql.fun.SqlCastFunction;
 import org.apache.calcite.sql.parser.SqlParserPos;
-import org.apache.calcite.sql.type.OperandTypes;
-import org.apache.calcite.sql.validate.SqlUserDefinedFunction;
 
-import com.linkedin.coral.com.google.common.base.Preconditions;
-import com.linkedin.coral.common.functions.FunctionReturnTypes;
 import com.linkedin.coral.common.transformers.SqlCallTransformer;
 import com.linkedin.coral.hive.hive2rel.functions.StaticHiveFunctionRegistry;
 
@@ -36,21 +31,6 @@ public class ToDateOperatorTransformer extends SqlCallTransformer {
 
   private static final String TO_OPERATOR_NAME = "date";
   private static final int NUM_OPERANDS = 1;
-  private static final SqlOperator TIMESTAMP_OPERATOR =
-      new SqlUserDefinedFunction(new SqlIdentifier("timestamp", SqlParserPos.ZERO), FunctionReturnTypes.TIMESTAMP, null,
-          OperandTypes.STRING, null, null) {
-        @Override
-        public void unparse(SqlWriter writer, SqlCall call, int leftPrec, int rightPrec) {
-          // for timestamp operator, we need to construct `CAST(x AS TIMESTAMP)`
-          Preconditions.checkState(call.operandCount() == 1);
-          final SqlWriter.Frame frame = writer.startFunCall("CAST");
-          call.operand(0).unparse(writer, 0, 0);
-          writer.sep("AS");
-          writer.literal("TIMESTAMP");
-          writer.endFunCall(frame);
-        }
-      };
-
   private final boolean avoidTransformToDateUDF;
 
   public ToDateOperatorTransformer(boolean avoidTransformToDateUDF) {
@@ -65,10 +45,10 @@ public class ToDateOperatorTransformer extends SqlCallTransformer {
 
   @Override
   protected SqlCall transform(SqlCall sqlCall) {
-    List<SqlNode> sourceOperands = sqlCall.getOperandList();
     List<SqlNode> newOperands = new ArrayList<>();
-    SqlNode timestampSqlCall = createCall(TIMESTAMP_OPERATOR, sourceOperands, SqlParserPos.ZERO);
-    newOperands.add(timestampSqlCall);
+    SqlCall sqlCastCall = new SqlCastFunction().createCall(SqlParserPos.ZERO, sqlCall.getOperandList().get(0),
+        SqlLiteral.createCharString("TIMESTAMP", SqlParserPos.ZERO));
+    newOperands.add(sqlCastCall);
     return createCall(createSqlOperator(TO_OPERATOR_NAME,
         HIVE_FUNCTION_REGISTRY.lookup(FROM_OPERATOR_NAME).iterator().next().getSqlOperator().getReturnTypeInference()),
         newOperands, SqlParserPos.ZERO);
