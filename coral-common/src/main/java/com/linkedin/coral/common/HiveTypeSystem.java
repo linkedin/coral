8a5a2889ba5850f164c7319dfbf602f807712372
Jiefan Li <jiefli@linkedin.com>
2023-05-01 16:06:05 -0400

    [Coral-Common] Modify return type of operator "/" (#401)

--- coral-common/src/main/java/com/linkedin/coral/common/HiveTypeSystem.java
+++ coral-common/src/main/java/com/linkedin/coral/common/HiveTypeSystem.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017-2022 LinkedIn Corporation. All rights reserved.
+ * Copyright 2017-2023 LinkedIn Corporation. All rights reserved.
  * Licensed under the BSD-2 Clause license.
  * See LICENSE in the project root for license information.
  */
@@ -9,6 +9,7 @@ import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.rel.type.RelDataTypeSystemImpl;
 import org.apache.calcite.sql.type.SqlTypeName;
+import org.apache.calcite.sql.type.SqlTypeUtil;
 
 
 // Copied from Hive source code
@@ -159,6 +160,19 @@ public class HiveTypeSystem extends RelDataTypeSystemImpl {
   @Override
   public boolean isSchemaCaseSensitive() {
     return false;
+  }
+
+  /**
+   * This method needs to be overridden to make sure that the "/" operator returns {@link org.apache.calcite.sql.type.ReturnTypes#DOUBLE}
+   * if neither of the operands is decimal type, which is compatible with the expected data type in Hive/Spark.
+   */
+  @Override
+  public RelDataType deriveDecimalDivideType(RelDataTypeFactory typeFactory, RelDataType type1, RelDataType type2) {
+    if (SqlTypeUtil.isDecimal(type1) || SqlTypeUtil.isDecimal(type2)) {
+      return super.deriveDecimalDivideType(typeFactory, type1, type2);
+    } else {
+      return nullableType(typeFactory, SqlTypeName.DOUBLE);
+    }
   }
 
   private RelDataType nullableType(RelDataTypeFactory typeFactory, SqlTypeName typeName) {
