d7d1e2cdbfb7b445833ff0270db7071122e4bc75
Walaa Eldin Moustafa <wmoustafa@linkedin.com>
2023-05-02 14:48:05 -0700

    Add Json-based tests and add lateral view test case

--- coral-visualization/src/test/java/com/linkedin/coral/vis/VisualizationUtilTest.java
+++ coral-visualization/src/test/java/com/linkedin/coral/vis/VisualizationUtilTest.java
@@ -5,10 +5,23 @@
  */
 package com.linkedin.coral.vis;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import guru.nidi.graphviz.attribute.Font;
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
+import guru.nidi.graphviz.model.Factory;
+import guru.nidi.graphviz.model.Graph;
+import guru.nidi.graphviz.model.Node;
 import java.io.File;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Map;
 import java.util.UUID;
 
+import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.util.SqlVisitor;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.metastore.api.MetaException;
 import org.apache.hadoop.hive.ql.metadata.Hive;
@@ -25,18 +38,89 @@ public class VisualizationUtilTest {
   private static final String CORAL_VISUALIZATION_TEST_DIR = "coral.visualization.test.dir";
 
   @Test
-  public void testBasicQuery() {
+  public void testRenderToFile() {
+    String[] queries = new String[] {
+        "SELECT * FROM foo, bar WHERE a = 1",
+        "SELECT key, value FROM (SELECT MAP('key1', 'value1') as m) tmp LATERAL VIEW EXPLODE(m) m_alias AS key, value"
+    };
     File imagesTempDir = new File(System.getProperty("java.io.tmpdir") + "/images" + UUID.randomUUID());
     HiveMscAdapter mscAdapter = createMscAdapter();
     HiveToRelConverter converter = new HiveToRelConverter(mscAdapter);
     VisualizationUtil visualizationUtil = VisualizationUtil.create(imagesTempDir);
-    visualizationUtil.visualizeCoralSqlNodeToFile(converter.toSqlNode("SELECT * FROM foo, bar WHERE a = 1"),
-        "/test1.png");
-    // For now we test the presence of the file.
-    // In the future, we can add tests to validate the structure of the generated graph by inspecting the corresponding
-    // JSON file.
-    assertEquals(imagesTempDir.list().length, 1);
+    for (int i = 0; i < queries.length; i++) {
+      visualizationUtil.visualizeCoralSqlNodeToFile(converter.toSqlNode(queries[i]), "/test" + i + ".png");
+    }
+    assertEquals(imagesTempDir.list().length, 2);
     imagesTempDir.delete();
+  }
+
+  @Test
+  public void testBasicQueryJson() {
+    JsonObject jsonObject = getJsonObject("SELECT * FROM foo, bar WHERE a = 1");
+    assertTrue(jsonLabelsExist(jsonObject, "SELECT", "JOIN", "=", "LIST", "foo", "bar", "a", "1", "*"));
+  }
+
+  @Test
+  public void testLateralJoinQueryJson() {
+    JsonObject jsonObject = getJsonObject(
+        "SELECT key, value FROM (SELECT MAP('key1', 'value1') as m) tmp LATERAL VIEW EXPLODE(m) m_alias AS key, value");
+    assertTrue(jsonLabelsExist(jsonObject, "SELECT", "JOIN", "LATERAL", "UNNEST", "AS", "MAP", "LIST"));
+  }
+
+  private JsonObject getJsonObject(String s) {
+    HiveMscAdapter mscAdapter = createMscAdapter();
+    HiveToRelConverter converter = new HiveToRelConverter(mscAdapter);
+    String jsonString = convertToGraphVisJsonString(converter.toSqlNode(s));
+    Gson gson = new Gson();
+    return gson.fromJson(jsonString, JsonObject.class);
+  }
+
+  private static String convertToGraphVisJsonString(SqlNode sqlNode) {
+    SqlVisitor<Node> sqlVisitor = new SqlNodeVisualizationVisitor();
+    Node node = sqlNode.accept(sqlVisitor);
+    Graph graph =
+        Factory.graph("test").directed().nodeAttr().with(Font.size(10)).linkAttr().with(Font.size(10)).with(node);
+    return Graphviz.fromGraph(graph).width(1000).render(Format.JSON).toString();
+  }
+
+  private static boolean jsonLabelsExist(Object jsonObject, String... labels) {
+    String[] keys = new String[labels.length];
+    Arrays.fill(keys, "label");
+    return jsonKeyValuePairsExist(jsonObject, keys, labels);
+  }
+
+  private static boolean jsonKeyValuePairsExist(Object object, String[] keys, String[] values) {
+    for (int i = 0; i < keys.length; i++) {
+      if (!jsonKeyValueExists(object, keys[i], values[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private static boolean jsonKeyValueExists(Object object, String key, String value) {
+    if (object instanceof JsonObject) {
+      JsonObject jsonObject = (JsonObject) object;
+      if (jsonObject.has(key)) {
+        if (jsonObject.get(key).getAsString().equals(value)) {
+          return true;
+        }
+      } else {
+        for (Map.Entry entry : jsonObject.entrySet()) {
+          if (jsonKeyValueExists(entry.getValue(), key, value)) {
+            return true;
+          }
+        }
+      }
+    } else if (object instanceof JsonArray) {
+      JsonArray jsonArray = (JsonArray) object;
+      for (Object element : jsonArray) {
+        if (jsonKeyValueExists(element, key, value)) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 
   private static HiveMscAdapter createMscAdapter() {
