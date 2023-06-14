359c08fc1b49038bd31ecd66598201df591b9539
aastha25 <aagrrawal@linkedin.com>
2023-06-14 10:46:45 -0700

    refactored transformations

--- coral-trino/src/main/java/com/linkedin/coral/trino/rel2trino/transformers/FromUtcTimestampOperatorTransformer.java
+++ coral-trino/src/main/java/com/linkedin/coral/trino/rel2trino/transformers/FromUtcTimestampOperatorTransformer.java
@@ -5,7 +5,10 @@
  */
 package com.linkedin.coral.trino.rel2trino.transformers;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.sql.SqlBasicTypeNameSpec;
@@ -16,6 +19,7 @@ import org.apache.calcite.sql.SqlOperator;
 import org.apache.calcite.sql.SqlTypeNameSpec;
 import org.apache.calcite.sql.fun.SqlStdOperatorTable;
 import org.apache.calcite.sql.type.BasicSqlType;
+import org.apache.calcite.sql.type.SqlTypeName;
 
 import com.linkedin.coral.common.HiveTypeSystem;
 import com.linkedin.coral.common.calcite.CalciteUtil;
@@ -32,13 +36,30 @@ import static org.apache.calcite.sql.type.SqlTypeName.DOUBLE;
 import static org.apache.calcite.sql.type.SqlTypeName.INTEGER;
 
 
+/**
+ * This transformer operates on SqlCalls with 'FROM_UTC_TIMESTAMP(x, timezone)' Coral IR function
+ * and transforms it into different trino engine compatible functions based on the data type of 'x'.
+ * For Example:
+ * A SqlCall of the form: "FROM_UTC_TIMESTAMP(10000, 'America/Los_Angeles')" is transformed to
+ * "CAST(AT_TIMEZONE(FROM_UNIXTIME_NANOS(CAST(10000 AS BIGINT) * 1000000), $CANONICALIZE_HIVE_TIMEZONE_ID('America/Los_Angeles')) AS TIMESTAMP(3))"
+ */
 public class FromUtcTimestampOperatorTransformer extends SqlCallTransformer {
+
+  private static final Set<SqlTypeName> INT_SQL_TYPE_NAMES =
+      new HashSet<>(Arrays.asList(BIGINT, INTEGER, SMALLINT, TINYINT));
+  private static final Set<SqlTypeName> DECIMAL_SQL_TYPE_NAMES = new HashSet<>(Arrays.asList(DOUBLE, FLOAT, DECIMAL));
+  private static final Set<SqlTypeName> TIMESTAMP_SQL_TYPE_NAMES = new HashSet<>(Arrays.asList(TIMESTAMP, DATE));
+  private static final String FROM_UTC_TIMESTAMP = "from_utc_timestamp";
+  private static final String AT_TIMEZONE = "at_timezone";
+  private static final String WITH_TIMEZONE = "with_timezone";
+  private static final String TO_UNIXTIME = "to_unixtime";
+  private static final String FROM_UNIXTIME_NANOS = "from_unixtime_nanos";
+  private static final String FROM_UNIXTIME = "from_unixtime";
+  private static final String CANONICALIZE_HIVE_TIMEZONE_ID = "$canonicalize_hive_timezone_id";
 
   public FromUtcTimestampOperatorTransformer(TypeDerivationUtil typeDerivationUtil) {
     super(typeDerivationUtil);
   }
-
-  private static final String FROM_UTC_TIMESTAMP = "from_utc_timestamp";
 
   @Override
   protected boolean condition(SqlCall sqlCall) {
@@ -51,66 +72,52 @@ public class FromUtcTimestampOperatorTransformer extends SqlCallTransformer {
     SqlNode sourceValue = operands.get(0);
     SqlNode timezone = operands.get(1);
 
-    RelDataType inputType = deriveRelDatatype(sqlCall.getOperandList().get(0));
     RelDataType targetType = new BasicSqlType(new HiveTypeSystem(), TIMESTAMP, 3);
+    RelDataType inputType = deriveRelDatatype(sqlCall.getOperandList().get(0));
+    SqlTypeName inputSqlTypeName = inputType.getSqlTypeName();
 
     // In below definitions we should use `TIMESTATMP WITH TIME ZONE`. As calcite is lacking
     // this type we use `TIMESTAMP` instead. It does not have any practical implications as result syntax tree
     // is not type-checked, and only used for generating output SQL for a view query.
-    SqlOperator trinoAtTimeZone = createSqlOperator("at_timezone", explicit(TIMESTAMP /* should be WITH TIME ZONE */));
+    SqlOperator trinoAtTimeZone = createSqlOperator(AT_TIMEZONE, explicit(TIMESTAMP /* should be WITH TIME ZONE */));
     SqlOperator trinoWithTimeZone =
-        createSqlOperator("with_timezone", explicit(TIMESTAMP /* should be WITH TIME ZONE */));
-    SqlOperator trinoToUnixTime = createSqlOperator("to_unixtime", explicit(DOUBLE));
+        createSqlOperator(WITH_TIMEZONE, explicit(TIMESTAMP /* should be WITH TIME ZONE */));
+    SqlOperator trinoToUnixTime = createSqlOperator(TO_UNIXTIME, explicit(DOUBLE));
     SqlOperator trinoFromUnixtimeNanos =
-        createSqlOperator("from_unixtime_nanos", explicit(TIMESTAMP /* should be WITH TIME ZONE */));
+        createSqlOperator(FROM_UNIXTIME_NANOS, explicit(TIMESTAMP /* should be WITH TIME ZONE */));
     SqlOperator trinoFromUnixTime =
-        createSqlOperator("from_unixtime", explicit(TIMESTAMP /* should be WITH TIME ZONE */));
-    SqlOperator trinoCanonicalizeHiveTimezoneId =
-        createSqlOperator("$canonicalize_hive_timezone_id", explicit(VARCHAR));
+        createSqlOperator(FROM_UNIXTIME, explicit(TIMESTAMP /* should be WITH TIME ZONE */));
+    SqlOperator trinoCanonicalizeHiveTimezoneId = createSqlOperator(CANONICALIZE_HIVE_TIMEZONE_ID, explicit(VARCHAR));
 
-    RelDataType bigintType = new BasicSqlType(new HiveTypeSystem(), BIGINT);
-    RelDataType doubleType = new BasicSqlType(new HiveTypeSystem(), DOUBLE);
-    SqlNode bigintConstant = CalciteUtil.createLiteralNumber(1000000, ZERO);
+    SqlCall canonicalizeHiveTimezoneIdSqlCall = trinoCanonicalizeHiveTimezoneId.createCall(ZERO, timezone);
 
-    if (inputType.getSqlTypeName() == BIGINT || inputType.getSqlTypeName() == INTEGER
-        || inputType.getSqlTypeName() == SMALLINT || inputType.getSqlTypeName() == TINYINT) {
+    if (INT_SQL_TYPE_NAMES.contains(inputSqlTypeName)) {
 
-      SqlCall i0 = SqlStdOperatorTable.CAST.createCall(ZERO, sourceValue, getSqlDataTypeSpecForCasting(bigintType));
-      SqlCall i1 = MULTIPLY.createCall(ZERO, i0, bigintConstant);
-      SqlCall i2 = trinoFromUnixtimeNanos.createCall(ZERO, i1);
+      SqlCall castedOperandCall = castOperand(sourceValue, new BasicSqlType(new HiveTypeSystem(), BIGINT));
+      SqlCall multipliedValueCall =
+          MULTIPLY.createCall(ZERO, castedOperandCall, CalciteUtil.createLiteralNumber(1000000, ZERO));
+      SqlCall fromUnixtimeNanosCall = trinoFromUnixtimeNanos.createCall(ZERO, multipliedValueCall);
+      SqlCall atTimeZoneCall =
+          trinoAtTimeZone.createCall(ZERO, fromUnixtimeNanosCall, canonicalizeHiveTimezoneIdSqlCall);
 
-      SqlCall inner = trinoCanonicalizeHiveTimezoneId.createCall(ZERO, timezone);
-      SqlCall i3 = trinoAtTimeZone.createCall(ZERO, i2, inner);
+      return castOperand(atTimeZoneCall, targetType);
+    } else if (DECIMAL_SQL_TYPE_NAMES.contains(inputSqlTypeName)) {
 
-      return castOperand(i3, targetType);
+      SqlCall castedOperandCall = castOperand(sourceValue, new BasicSqlType(new HiveTypeSystem(), DOUBLE));
+      SqlCall fromUnixTimeCall = trinoFromUnixTime.createCall(ZERO, castedOperandCall);
+      SqlCall atTimeZoneCall = trinoAtTimeZone.createCall(ZERO, fromUnixTimeCall, canonicalizeHiveTimezoneIdSqlCall);
+
+      return castOperand(atTimeZoneCall, targetType);
+    } else if (TIMESTAMP_SQL_TYPE_NAMES.contains(inputSqlTypeName)) {
+
+      SqlCall withTimeZoneCall =
+          trinoWithTimeZone.createCall(ZERO, sourceValue, CalciteUtil.createStringLiteral("UTC", ZERO));
+      SqlCall toUnixTimeCall = trinoToUnixTime.createCall(ZERO, withTimeZoneCall);
+      SqlCall fromUnixTimeCall = trinoFromUnixTime.createCall(ZERO, toUnixTimeCall);
+      SqlCall atTimeZoneCall = trinoAtTimeZone.createCall(ZERO, fromUnixTimeCall, canonicalizeHiveTimezoneIdSqlCall);
+
+      return castOperand(atTimeZoneCall, targetType);
     }
-
-    if (inputType.getSqlTypeName() == DOUBLE || inputType.getSqlTypeName() == FLOAT
-        || inputType.getSqlTypeName() == DECIMAL) {
-
-      SqlCall i0 = SqlStdOperatorTable.CAST.createCall(ZERO, sourceValue, getSqlDataTypeSpecForCasting(doubleType));
-
-      SqlCall i11 = trinoFromUnixTime.createCall(ZERO, i0);
-      SqlCall i12 = trinoCanonicalizeHiveTimezoneId.createCall(ZERO, timezone);
-
-      SqlCall i2 = trinoAtTimeZone.createCall(ZERO, i11, i12);
-
-      return castOperand(i2, targetType);
-    }
-
-    if (inputType.getSqlTypeName() == TIMESTAMP || inputType.getSqlTypeName() == DATE) {
-      SqlNode stringConstant = CalciteUtil.createStringLiteral("UTC", ZERO);
-
-      SqlCall i00 = trinoCanonicalizeHiveTimezoneId.createCall(ZERO, timezone);
-
-      SqlCall i0 = trinoWithTimeZone.createCall(ZERO, sourceValue, stringConstant);
-      SqlCall i1 = trinoToUnixTime.createCall(ZERO, i0);
-      SqlCall i2 = trinoFromUnixTime.createCall(ZERO, i1);
-      SqlCall i3 = trinoAtTimeZone.createCall(ZERO, i2, i00);
-
-      return castOperand(i3, targetType);
-    }
-
     return sqlCall;
   }
 
