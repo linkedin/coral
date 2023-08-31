1e0bb4a5d3aadee9e2f1053388e6637a885b0e15
Kevin Ge <kge@linkedin.com>
2023-08-31 16:08:14 -0400

    mix merge issues

--- coral-hive/src/main/java/com/linkedin/coral/hive/hive2rel/HiveConvertletTable.java
+++ coral-hive/src/main/java/com/linkedin/coral/hive/hive2rel/HiveConvertletTable.java
@@ -5,23 +5,18 @@
  */
 package com.linkedin.coral.hive.hive2rel;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rex.RexNode;
 import org.apache.calcite.sql.SqlCall;
 import org.apache.calcite.sql.SqlDataTypeSpec;
 import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.fun.SqlCastFunction;
-import org.apache.calcite.sql.fun.SqlStdOperatorTable;
 import org.apache.calcite.sql2rel.ReflectiveConvertletTable;
 import org.apache.calcite.sql2rel.SqlRexContext;
 import org.apache.calcite.sql2rel.SqlRexConvertlet;
 import org.apache.calcite.sql2rel.StandardConvertletTable;
 
 import com.linkedin.coral.common.functions.FunctionFieldReferenceOperator;
-import com.linkedin.coral.hive.hive2rel.functions.HiveNamedStructFunction;
 
 
 /**
@@ -29,17 +24,6 @@ import com.linkedin.coral.hive.hive2rel.functions.HiveNamedStructFunction;
  * @see ReflectiveConvertletTable documentation for method naming and visibility rules
  */
 public class HiveConvertletTable extends ReflectiveConvertletTable {
-
-  @SuppressWarnings("unused")
-  public RexNode convertNamedStruct(SqlRexContext cx, HiveNamedStructFunction func, SqlCall call) {
-    List<RexNode> operandExpressions = new ArrayList<>(call.operandCount() / 2);
-    for (int i = 0; i < call.operandCount(); i += 2) {
-      operandExpressions.add(cx.convertExpression(call.operand(i + 1)));
-    }
-    RelDataType retType = cx.getValidator().getValidatedNodeType(call);
-    RexNode rowNode = cx.getRexBuilder().makeCall(retType, SqlStdOperatorTable.ROW, operandExpressions);
-    return cx.getRexBuilder().makeCast(retType, rowNode);
-  }
 
   @SuppressWarnings("unused")
   public RexNode convertFunctionFieldReferenceOperator(SqlRexContext cx, FunctionFieldReferenceOperator op,
