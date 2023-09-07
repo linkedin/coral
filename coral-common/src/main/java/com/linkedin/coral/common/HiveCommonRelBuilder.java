01fc6bdede2f7c88d65247bd00e07edd0fd162dd
Yiqiang Ding <yiqding@Yiqiangs-MacBook-Pro.local>
2023-09-07 09:37:42 -0700

    fix format

--- coral-common/src/main/java/com/linkedin/coral/common/HiveCommonRelBuilder.java
+++ coral-common/src/main/java/com/linkedin/coral/common/HiveCommonRelBuilder.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2023 LinkedIn Corporation. All rights reserved.
+ * Licensed under the BSD-2 Clause license.
+ * See LICENSE in the project root for license information.
+ */
 package com.linkedin.coral.common;
 
 import org.apache.calcite.plan.Context;
@@ -8,15 +13,17 @@ import org.apache.calcite.tools.FrameworkConfig;
 import org.apache.calcite.tools.Frameworks;
 import org.apache.calcite.tools.RelBuilder;
 
-public class HiveCommonRelBuilder extends RelBuilder {
-    private HiveCommonRelBuilder(Context context, RelOptCluster cluster, RelOptSchema relOptSchema) {
-        super(context, cluster, relOptSchema);
-    }
 
-    public static RelBuilder create(FrameworkConfig config) {
-        return Frameworks.withPrepare(config, (cluster, relOptSchema, rootSchema,
-                statement) -> { cluster = RelOptCluster.create(cluster.getPlanner(),
-                new RexBuilder(new CoralJavaTypeFactoryImpl(cluster.getTypeFactory().getTypeSystem())));
-                return new HiveCommonRelBuilder(config.getContext(), cluster, relOptSchema);});
-    }
+public class HiveCommonRelBuilder extends RelBuilder {
+  private HiveCommonRelBuilder(Context context, RelOptCluster cluster, RelOptSchema relOptSchema) {
+    super(context, cluster, relOptSchema);
+  }
+
+  public static RelBuilder create(FrameworkConfig config) {
+    return Frameworks.withPrepare(config, (cluster, relOptSchema, rootSchema, statement) -> {
+      cluster = RelOptCluster.create(cluster.getPlanner(),
+          new RexBuilder(new CoralJavaTypeFactoryImpl(cluster.getTypeFactory().getTypeSystem())));
+      return new HiveCommonRelBuilder(config.getContext(), cluster, relOptSchema);
+    });
+  }
 }
