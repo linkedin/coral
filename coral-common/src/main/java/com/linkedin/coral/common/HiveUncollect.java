2f32b63aa919eee1abb846d2c1fb937458d26961
Yiqiang Ding <yiqding@Yiqiangs-MacBook-Pro.local>
2023-09-07 12:19:15 -0700

    merge the code of HiveCommonRelBuilder into HiveRelBuilder and move it to coral-common

--- coral-common/src/main/java/com/linkedin/coral/common/HiveUncollect.java
+++ coral-common/src/main/java/com/linkedin/coral/common/HiveUncollect.java
@@ -1,9 +1,9 @@
 /**
- * Copyright 2018-2022 LinkedIn Corporation. All rights reserved.
+ * Copyright 2018-2023 LinkedIn Corporation. All rights reserved.
  * Licensed under the BSD-2 Clause license.
  * See LICENSE in the project root for license information.
  */
-package com.linkedin.coral.hive.hive2rel.rel;
+package com.linkedin.coral.common;
 
 import java.util.List;
 
