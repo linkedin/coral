bc03dbd68470034d89886d0125942c70e2e499d0
Kevin Ge <kge@linkedin.com>
2023-10-05 18:21:05 -0400

    sans dm font theme

--- coral-service/frontend/src/app/layout.js
+++ coral-service/frontend/src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en' className='h-max bg-white'>
-      <body className={`h-max ${inter.className} font-courier`}>
+      <body className={`h-max ${inter.className} font-sans`}>
         {children}
       </body>
     </html>
--- coral-service/frontend/tailwind.config.js
+++ coral-service/frontend/tailwind.config.js
@@ -13,8 +13,7 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       fontFamily: {
-        // Add Courier New to the list of fonts
-        courier: ['Courier New', 'monospace'],
+        sans: ['"DM Sans"', 'sans-serif'],
       },
     },
   },
