03a29bbfbbc082b5293fbcd85b3c763c553cc8c1
aastha25 <aagrrawal@linkedin.com>
2023-12-13 11:58:27 -0800

    coral-hive spotlesscheck

--- coral-hive/src/main/java/com/linkedin/coral/hive/hive2rel/functions/OrdinalReturnTypeInferenceV2.java
+++ coral-hive/src/main/java/com/linkedin/coral/hive/hive2rel/functions/OrdinalReturnTypeInferenceV2.java
@@ -1,6 +1,12 @@
+/**
+ * Copyright 2023 LinkedIn Corporation. All rights reserved.
+ * Licensed under the BSD-2 Clause license.
+ * See LICENSE in the project root for license information.
+ */
 package com.linkedin.coral.hive.hive2rel.functions;
 
 import org.apache.calcite.sql.type.OrdinalReturnTypeInference;
+
 
 /**
  * Custom implementation of {@link OrdinalReturnTypeInference} which allows inferring the return type
