1d60f28793ac1fd0ba4226c74671fd97b37d88eb
yyy1000 <junhaoliu2023@gmail.com>
2024-07-15 10:09:16 -0700

    test: unify test casts

--- coral-incremental/src/test/java/com/linkedin/coral/incremental/RelNodeGenerationTest.java
+++ coral-incremental/src/test/java/com/linkedin/coral/incremental/RelNodeGenerationTest.java
@@ -95,115 +95,29 @@ public class RelNodeGenerationTest {
 
   @Test
   public void testSimpleSelectAll() {
-    String sql = "SELECT * FROM test.foo";
-    // assertEquals(getIncrementalModification(sql), sql);
+    String sql = "SELECT * FROM test.bar1";
+    String incrementalSql = "SELECT *\n" + "FROM test.bar1_delta AS bar1_delta";
+    estimator.setIOCostParam(2.0);
+    estimator.loadStatistic(TEST_JSON_FILE_DIR + "statistic.json");
+    assertEquals(getIncrementalModification(sql), incrementalSql);
+    estimator.setStat(fakeStatData());
+    assertEquals(getIncrementalModification(sql), incrementalSql);
   }
 
   @Test
   public void testSimpleJoin() {
     String sql = "SELECT * FROM test.bar1 JOIN test.bar2 ON test.bar1.x = test.bar2.x";
     String prevSql = "SELECT *\n" + "FROM test.bar1 AS bar1\n" + "INNER JOIN test.bar2 AS bar2 ON bar1.x = bar2.x";
-    String expected = "SELECT *\n" + "FROM (SELECT *\n" + "FROM test.bar1_prev AS bar1_prev\n"
+    String incrementalSql = "SELECT *\n" + "FROM (SELECT *\n" + "FROM test.bar1_prev AS bar1_prev\n"
         + "INNER JOIN test.bar2_delta AS bar2_delta ON bar1_prev.x = bar2_delta.x\n" + "UNION ALL\n" + "SELECT *\n"
         + "FROM test.bar1_delta AS bar1_delta\n"
         + "INNER JOIN test.bar2_prev AS bar2_prev ON bar1_delta.x = bar2_prev.x) AS t\n" + "UNION ALL\n" + "SELECT *\n"
         + "FROM test.bar1_delta AS bar1_delta0\n"
         + "INNER JOIN test.bar2_delta AS bar2_delta0 ON bar1_delta0.x = bar2_delta0.x";
-    estimator.loadStatistic(TEST_JSON_FILE_DIR + "statistic.json");
     estimator.setIOCostParam(2.0);
-    assertEquals(getIncrementalModification(sql), expected);
+    estimator.loadStatistic(TEST_JSON_FILE_DIR + "statistic.json");
+    assertEquals(getIncrementalModification(sql), incrementalSql);
     estimator.setStat(fakeStatData());
     assertEquals(getIncrementalModification(sql), prevSql);
-  }
-
-  @Test
-  public void testJoinCost() {
-    String sql = "SELECT * FROM test.bar1 JOIN test.bar2 ON test.bar1.x = test.bar2.x";
-    String expected = "SELECT *\n" + "FROM (SELECT *\n" + "FROM test.bar1_prev AS bar1_prev\n"
-        + "INNER JOIN test.bar2_delta AS bar2_delta ON bar1_prev.x = bar2_delta.x\n" + "UNION ALL\n" + "SELECT *\n"
-        + "FROM test.bar1_delta AS bar1_delta\n"
-        + "INNER JOIN test.bar2_prev AS bar2_prev ON bar1_delta.x = bar2_prev.x) AS t\n" + "UNION ALL\n" + "SELECT *\n"
-        + "FROM test.bar1_delta AS bar1_delta0\n"
-        + "INNER JOIN test.bar2_delta AS bar2_delta0 ON bar1_delta0.x = bar2_delta0.x";
-    // assertEquals(getIncrementalModification(sql), expected);
-  }
-
-  @Test
-  public void testJoinWithFilter() {
-    String sql = "SELECT * FROM test.bar1 JOIN test.bar2 ON test.bar1.x = test.bar2.x WHERE test.bar1.x > 10";
-    String expected = "SELECT *\n" + "FROM (SELECT *\n" + "FROM (SELECT *\n" + "FROM test.bar1_prev AS bar1_prev\n"
-        + "INNER JOIN test.bar2_delta AS bar2_delta ON bar1_prev.x = bar2_delta.x\n" + "UNION ALL\n" + "SELECT *\n"
-        + "FROM test.bar1_delta AS bar1_delta\n"
-        + "INNER JOIN test.bar2_prev AS bar2_prev ON bar1_delta.x = bar2_prev.x) AS t\n" + "UNION ALL\n" + "SELECT *\n"
-        + "FROM test.bar1_delta AS bar1_delta0\n"
-        + "INNER JOIN test.bar2_delta AS bar2_delta0 ON bar1_delta0.x = bar2_delta0.x) AS t0\n" + "WHERE t0.x > 10";
-    // assertEquals(getIncrementalModification(sql), expected);
-  }
-
-  @Test
-  public void testJoinWithNestedFilter() {
-    String sql =
-        "WITH tmp AS (SELECT * from test.bar1 WHERE test.bar1.x > 10), tmp2 AS (SELECT * from test.bar2) SELECT * FROM tmp JOIN tmp2 ON tmp.x = tmp2.x";
-    String expected = "SELECT *\n" + "FROM (SELECT *\n" + "FROM (SELECT *\n" + "FROM test.bar1_prev AS bar1_prev\n"
-        + "WHERE bar1_prev.x > 10) AS t\n" + "INNER JOIN test.bar2_delta AS bar2_delta ON t.x = bar2_delta.x\n"
-        + "UNION ALL\n" + "SELECT *\n" + "FROM (SELECT *\n" + "FROM test.bar1_delta AS bar1_delta\n"
-        + "WHERE bar1_delta.x > 10) AS t0\n" + "INNER JOIN test.bar2_prev AS bar2_prev ON t0.x = bar2_prev.x) AS t1\n"
-        + "UNION ALL\n" + "SELECT *\n" + "FROM (SELECT *\n" + "FROM test.bar1_delta AS bar1_delta0\n"
-        + "WHERE bar1_delta0.x > 10) AS t2\n" + "INNER JOIN test.bar2_delta AS bar2_delta0 ON t2.x = bar2_delta0.x";
-    // assertEquals(getIncrementalModification(sql), expected);
-  }
-
-  @Test
-  public void testNestedJoin() {
-    String sql =
-        "WITH tmp AS (SELECT * FROM test.bar1 INNER JOIN test.bar2 ON test.bar1.x = test.bar2.x) SELECT * FROM tmp INNER JOIN test.bar3 ON tmp.x = test.bar3.x";
-    String expected = "SELECT *\n" + "FROM (SELECT *\n" + "FROM test.bar1_prev AS bar1_prev\n"
-        + "INNER JOIN test.bar2_prev AS bar2_prev ON bar1_prev.x = bar2_prev.x\n"
-        + "INNER JOIN test.bar3_delta AS bar3_delta ON bar1_prev.x = bar3_delta.x\n" + "UNION ALL\n" + "SELECT *\n"
-        + "FROM (SELECT *\n" + "FROM (SELECT *\n" + "FROM test.bar1_prev AS bar1_prev0\n"
-        + "INNER JOIN test.bar2_delta AS bar2_delta ON bar1_prev0.x = bar2_delta.x\n" + "UNION ALL\n" + "SELECT *\n"
-        + "FROM test.bar1_delta AS bar1_delta\n"
-        + "INNER JOIN test.bar2_prev AS bar2_prev0 ON bar1_delta.x = bar2_prev0.x) AS t\n" + "UNION ALL\n"
-        + "SELECT *\n" + "FROM test.bar1_delta AS bar1_delta0\n"
-        + "INNER JOIN test.bar2_delta AS bar2_delta0 ON bar1_delta0.x = bar2_delta0.x) AS t0\n"
-        + "INNER JOIN test.bar3_prev AS bar3_prev ON t0.x = bar3_prev.x) AS t1\n" + "UNION ALL\n" + "SELECT *\n"
-        + "FROM (SELECT *\n" + "FROM (SELECT *\n" + "FROM test.bar1_prev AS bar1_prev1\n"
-        + "INNER JOIN test.bar2_delta AS bar2_delta1 ON bar1_prev1.x = bar2_delta1.x\n" + "UNION ALL\n" + "SELECT *\n"
-        + "FROM test.bar1_delta AS bar1_delta1\n"
-        + "INNER JOIN test.bar2_prev AS bar2_prev1 ON bar1_delta1.x = bar2_prev1.x) AS t2\n" + "UNION ALL\n"
-        + "SELECT *\n" + "FROM test.bar1_delta AS bar1_delta2\n"
-        + "INNER JOIN test.bar2_delta AS bar2_delta2 ON bar1_delta2.x = bar2_delta2.x) AS t3\n"
-        + "INNER JOIN test.bar3_delta AS bar3_delta0 ON t3.x = bar3_delta0.x";
-    // assertEquals(getIncrementalModification(sql), expected);
-  }
-
-  @Test
-  public void testUnion() {
-    String sql = "SELECT * FROM test.bar1 UNION SELECT * FROM test.bar2 UNION SELECT * FROM test.bar3";
-    String expected =
-        "SELECT t1.x, t1.y\n" + "FROM (SELECT t.x, t.y\n" + "FROM (SELECT *\n" + "FROM test.bar1_delta AS bar1_delta\n"
-            + "UNION ALL\n" + "SELECT *\n" + "FROM test.bar2_delta AS bar2_delta) AS t\n" + "GROUP BY t.x, t.y\n"
-            + "UNION ALL\n" + "SELECT *\n" + "FROM test.bar3_delta AS bar3_delta) AS t1\n" + "GROUP BY t1.x, t1.y";
-    // assertEquals(getIncrementalModification(sql), expected);
-  }
-
-  @Test
-  public void testSelectSpecific() {
-    String sql = "SELECT a FROM test.foo";
-    String expected = "SELECT foo_delta.a\n" + "FROM test.foo_delta AS foo_delta";
-    // assertEquals(getIncrementalModification(sql), expected);
-  }
-
-  @Test
-  public void testSelectSpecificJoin() {
-    String sql = "SELECT test.bar2.y FROM test.bar1 JOIN test.bar2 ON test.bar1.x = test.bar2.x";
-    String expected =
-        "SELECT t0.y0 AS y\n" + "FROM (SELECT *\n" + "FROM (SELECT *\n" + "FROM test.bar1_prev AS bar1_prev\n"
-            + "INNER JOIN test.bar2_delta AS bar2_delta ON bar1_prev.x = bar2_delta.x\n" + "UNION ALL\n" + "SELECT *\n"
-            + "FROM test.bar1_delta AS bar1_delta\n"
-            + "INNER JOIN test.bar2_prev AS bar2_prev ON bar1_delta.x = bar2_prev.x) AS t\n" + "UNION ALL\n"
-            + "SELECT *\n" + "FROM test.bar1_delta AS bar1_delta0\n"
-            + "INNER JOIN test.bar2_delta AS bar2_delta0 ON bar1_delta0.x = bar2_delta0.x) AS t0";
-    // assertEquals(getIncrementalModification(sql), expected);
   }
 }
