bf4cae786c555016346dded1bfb9d62c98773708
Athrey Yadatore Venkateshababu <avenkateshababu@linkedin.com>
2025-01-17 15:11:29 -0800

    remove sensitive fields

--- coral-dbt/src/main/resources/macros/coral_macros/default/utils/last_n_hours.sql
+++ coral-dbt/src/main/resources/macros/coral_macros/default/utils/last_n_hours.sql
@@ -1,5 +1,5 @@
 {% macro last_n_hours(hours=8) %}
-{%- set current_datetime_pst = in_dbt_utils.logical_date(timezone="US/Pacific") -%}
-{%- set target_datetime_pst =  current_datetime_pst - modules.datetime.timedelta(hours=hours) -%}
-"{{ target_datetime_pst.strftime('%Y-%m-%d-%H') }}"
+    {%- set current_datetime_pst = in_dbt_utils.logical_date(timezone="US/Pacific") -%}
+    {%- set target_datetime_pst =  current_datetime_pst - modules.datetime.timedelta(hours=hours) -%}
+    "{{ target_datetime_pst.strftime('%Y-%m-%d-%H') }}"
 {% endmacro %}
