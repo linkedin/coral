db3c619cfae553850b81be4646b746208b4c4190
Athrey Yadatore Venkateshababu <avenkateshababu@linkedin.com>
2025-01-17 15:12:00 -0800

    remove sensitive fields

--- coral-dbt/src/main/resources/macros/coral_macros/default/utils/last_n_hours.sql
+++ coral-dbt/src/main/resources/macros/coral_macros/default/utils/last_n_hours.sql
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2023 LinkedIn Corporation. All rights reserved.
+ * Licensed under the BSD-2 Clause license.
+ * See LICENSE in the project root for license information.
+ */
 {% macro last_n_hours(hours=8) %}
     {%- set current_datetime_pst = in_dbt_utils.logical_date(timezone="US/Pacific") -%}
     {%- set target_datetime_pst =  current_datetime_pst - modules.datetime.timedelta(hours=hours) -%}
