f70d9a8b8a5b404528b21901672bcea354aa6747
Limian Zhang <raylmzhang@gmail.com>
2025-07-14 10:47:51 -0700

    done for incremental

--- coral-incremental/src/test/java/com/linkedin/coral/incremental/RelToIncrementalSqlConverterTest.java
+++ coral-incremental/src/test/java/com/linkedin/coral/incremental/RelToIncrementalSqlConverterTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2023 LinkedIn Corporation. All rights reserved.
+ * Copyright 2023-2025 LinkedIn Corporation. All rights reserved.
  * Licensed under the BSD-2 Clause license.
  * See LICENSE in the project root for license information.
  */
@@ -8,8 +8,9 @@ package com.linkedin.coral.incremental;
 import java.io.File;
 import java.io.IOException;
 
-import org.apache.calcite.rel.RelNode;
-import org.apache.calcite.sql.SqlNode;
+import com.linkedin.relocated.org.apache.calcite.rel.RelNode;
+import com.linkedin.relocated.org.apache.calcite.sql.SqlNode;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.metastore.api.MetaException;
@@ -21,7 +22,7 @@ import org.testng.annotations.Test;
 import com.linkedin.coral.transformers.CoralRelToSqlNodeConverter;
 
 import static com.linkedin.coral.incremental.TestUtils.*;
-import static org.apache.calcite.sql.type.OperandTypes.*;
+import static com.linkedin.relocated.org.apache.calcite.sql.type.OperandTypes.*;
 import static org.testng.Assert.*;
 
 
